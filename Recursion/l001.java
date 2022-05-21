82e6aa921ed1ddf06874ddd276f94617439391ea
Shubhra Sachdev <shubhra99sachdev@gmail.com>
2022-05-22 04:49:32 +0530

    Update

--- Recursion/l001.java
+++ Recursion/l001.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class l001 {    
-    // recursion lecture 1
+    // recursion lecture 1 - Jan 8, 2021
     // print increasing numbers from a to b
     void printIncreasing(int a, int b){
         if(a == b + 1)
@@ -39,7 +39,7 @@ class l001 {
         return n * factorial(n - 1);
     }
 
-    // recursion lecture 2    
+    // recursion lecture 2 - Jan 10, 2021   
     // calculate x raised to power n in linear time
     int power(int x,int n){
         if(n == 0) return 1;
@@ -89,7 +89,7 @@ class l001 {
     }
   
     // find first index of an element in an array
-    int firstIndex(int arr[], int idx, int x, int n){
+    int firstIndex(int[] arr, int idx, int x, int n){
         if(idx == n) return -1;
         if(arr[idx] == x) return idx;
         return firstIndex(arr, idx + 1, x, n);
@@ -106,7 +106,7 @@ class l001 {
         return prev;
     }
 
-    // recursion lecture 3
+    // recursion lecture 3 - Jan 11, 2021
     // pattern 1
     // pattern 2
     // check if arr is palindrome
@@ -133,7 +133,7 @@ class l001 {
         arr[val] = i;
     }
 
-    // recursion - homework
+    // recursion - homework - https://www.hackerrank.com/contests/pepdec62017/challenges/filters/page:8
     /* 16. Sum of Digits in a string 
         Input   : "1234"
         Output  : 10
@@ -200,6 +200,202 @@ class l001 {
 
     // 21. Remove adjacent duplicate characters from string
     // Eg PEPPEP -> PEPEP
+    
+    // Going down
+    public static String removeDuplicates(String str){
+        if(str.length() == 1) return str;
+        char ch = str.charAt(0);
+        String ros = str.substring(1);
+        String res = removeDuplicates(ros);
+        if(ch == res.charAt(0)) return res;
+        else return ch + res;
+    }
+
+    // Going up
+    public static void removeDuplicates_wayUp(String str, int idx, String ans){
+        if(idx == str.length() - 1){
+            System.out.println(ans + str.charAt(idx));
+            return;
+        }
+        char ch = str.charAt(idx);
+        if(ch == str.charAt(idx + 1)) removeDuplicates_wayUp(str, idx + 1, ans);
+        else removeDuplicates_wayUp(str, idx + 1, ans + ch);
+    }
+
+    // 23. Count or Remove Hi
+    // Eg hihihijjhihi -> Num of Hi = 5, w/o Hi string becomes "jj"
+    
+    // Counts num of hi in string
+    public static int countHi(String str, int idx){
+        if(idx >= str.length() - 1) return 0;
+        String s = str.substring(idx, idx + 2);
+        if(s.equals("hi")) return countHi(str, idx + 2) + 1;
+        return countHi(str, idx + 1);
+    }
+    
+    // Going up - prints string w/o hi
+    public static void removeHi(String str, int idx, String ans){
+        if(idx >= str.length() - 1){
+            if(idx == str.length() - 1) ans += str.charAt(idx);
+            System.out.println(ans);
+            return;
+        }
+        String s = str.substring(idx, idx + 2);
+        if(s.equals("hi")) removeHi(str, idx + 2, ans);
+        else removeHi(str, idx + 1, ans + str.charAt(idx));
+    }
+    
+    // Going down - returns string w/o hi
+    public static String removeHi(String str, int idx){
+        if(idx == str.length() - 1) return str.charAt(idx) + "";
+        if(idx > str.length() - 1) return "";
+        String s = str.substring(idx, idx + 2);
+        if(s.equals("hi")) return removeHi(str, idx + 2);
+        return str.charAt(idx) + removeHi(str, idx + 1);
+    }
+
+    // 24. Replace hi with pep
+    // Eg hihihijjhihi -> peppeppepjjpeppep
+
+    // Going down
+    public static String hiToPep(String str, int idx) {
+        if(idx == str.length() - 1) return str.charAt(idx) + "";
+        if(idx > str.length() - 1) return "";
+        String s = str.substring(idx, idx + 2);
+        if(s.equals("hi")) return "pep" + hiToPep(str, idx + 2);
+        return str.charAt(idx) + hiToPep(str, idx + 1);
+    }  
+
+    // Going up
+    public static void hiToPep(String str, int idx, String ans) {
+        if(idx >= str.length() - 1) {
+            if(idx == str.length() - 1) ans += str.charAt(idx);
+            System.out.println(ans);
+            return;
+        }
+        String s = str.substring(idx, idx + 2);
+        if(s.equals("hi")) hiToPep(str, idx + 2, ans + "pep");
+        else hiToPep(str, idx + 1, ans + str.charAt(idx));
+    }
+
+    // 25. Remove "hi", unless it is followed by t / Remove all hi except the "hi" in "hit" - also count "hi"s which are not hit 
+    // Eg hihihithi -> hit
+    // 1 test case failed for this
+
+    // Count hi without hit
+    public static int countHiWithoutHit(String str, int idx) {
+        if(idx >= str.length() - 1) return 0;
+        if(idx == str.length() - 2) {
+            if(str.charAt(idx) == 'h' && str.charAt(idx + 1) == 'i') return 1;
+            return 0;
+        }
+        String s = str.substring(idx, idx + 2);
+        if(s.equals("hi")) {
+            if(str.charAt(idx + 2) == 't') return countHiWithoutHit(str, idx + 3);
+            return 1 + countHiWithoutHit(str, idx + 2);
+        }
+        return countHiWithoutHit(str, idx + 1);
+    }
+    
+    // Going down
+    public static String removeHiWithoutHit(String str, int idx) {
+        if(idx > str.length() - 1) return "";
+        if(idx >= str.length() - 2) {
+            String base = "";
+            base += str.charAt(idx);
+            if(idx == str.length() - 1) return base;
+            base += str.charAt(idx + 1);
+            if(base.equals("hi")) return "";
+            return base;
+        }
+        String s = str.substring(idx, idx + 2);
+        if(s.equals("hi")) {
+            if(str.charAt(idx + 2) == 't') return "hit" + removeHiWithoutHit(str, idx + 3);
+            return removeHiWithoutHit(str, idx + 2);
+        }
+        return str.charAt(idx) + removeHiWithoutHit(str, idx + 1);
+    }
+
+    // Going up
+    public static void removeHiWithoutHit(String str, int idx, String ans) {
+        if(idx > str.length() - 1) System.out.println(ans);
+        if(idx >= str.length() - 2) {
+            if(idx == str.length() - 1) {
+                System.out.println(ans + str.charAt(idx));
+                return;
+            }
+            if(str.charAt(idx) != 'h' || str.charAt(idx + 1) != 'i') ans += str.charAt(idx) + str.charAt(idx + 1);
+            System.out.println(ans);
+            return;
+        }
+        String s = str.substring(idx, idx + 2);
+        if(s.equals("hi")) {
+            if(str.charAt(idx + 2) == 't') removeHiWithoutHit(str, idx + 3, ans + "hit");
+            else removeHiWithoutHit(str, idx + 2, ans);
+        }
+        else removeHiWithoutHit(str, idx + 1, ans + str.charAt(idx));
+    }
+
+    // 26. Replace hi with pep except for hit
+    // Eg hihihithi -> peppephitpep
+    // Going down
+    public static String replaceHiWithoutHit(String str, int idx) {
+        if(idx > str.length() - 1) return "";
+        if(idx >= str.length() - 2) {
+            String base = "";
+            base += str.charAt(idx);
+            if(idx == str.length() - 1) return base;
+            base += str.charAt(idx + 1);
+            if(base.equals("hi")) return "pep";
+            return base;
+        }
+        String s = str.substring(idx, idx + 2);
+        if(s.equals("hi")) {
+            if(str.charAt(idx + 2) == 't') return "hit" + replaceHiWithoutHit(str, idx + 3);
+            return "pep" + replaceHiWithoutHit(str, idx + 2);
+        }
+        return str.charAt(idx) + replaceHiWithoutHit(str, idx + 1);
+    }
+
+    // Going up
+    public static void replaceHiWithoutHit(String str, int idx, String ans) {
+        if(idx > str.length() - 1) System.out.println(ans);
+        if(idx >= str.length() - 2) {
+            if(idx == str.length() - 1) {
+                System.out.println(ans + str.charAt(idx));
+                return;
+            }
+            if(str.charAt(idx) == 'h' && str.charAt(idx + 1) == 'i') ans += "pep"; 
+            else ans += str.charAt(idx) + str.charAt(idx + 1);
+            System.out.println(ans);
+            return;
+        }
+        String s = str.substring(idx, idx + 2);
+        if(s.equals("hi")) {
+            if(str.charAt(idx + 2) == 't') replaceHiWithoutHit(str, idx + 3, ans + "hit");
+            else replaceHiWithoutHit(str, idx + 2, ans + "pep");
+        }
+        else replaceHiWithoutHit(str, idx + 1, ans + str.charAt(idx));
+    }
+
+    // 29. Number of 'aaa' in string
+
+    // with overlap
+    // Eg- aaaaaa -> 4
+    public static int numOfPattern(String str, int idx) {
+        if(idx >= str.length() - 2) return 0;
+        int ans = numOfPattern(str, idx + 1);
+        if(str.charAt(idx) == 'a' && str.charAt(idx + 1) == 'a' && str.charAt(idx + 2) == 'a') ans++;
+        return ans;
+    }
+    
+    // without overlap
+    // Eg- aaaaaa -> 2
+    public static int numOfPatternNoOverlap(String str, int idx) {
+        if(idx >= str.length() - 2) return 0;
+        if(str.charAt(idx) == 'a' && str.charAt(idx + 1) == 'a' && str.charAt(idx + 2) == 'a')  return 1 + numOfPatternNoOverlap(str, idx + 3);
+        return numOfPatternNoOverlap(str, idx + 1);
+    }
 
 
     // printing subsequences for a given string - method 1
